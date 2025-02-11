0d7b503bf466456cb8eb04b5709f28c79187584a
Renzo Reyna <reynarenzo.88@gmail.com>
2025-02-11 11:39:45 -0300

    app de streamlit para visualizar el valor de monedas de países sudamericanos en USD

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Descripción del Programa:
+
+### Esta aplicación desarrollada en Python con Streamlit permite visualizar el valor de las monedas de diversos países sudamericanos en relación con el dólar estadounidense. A través de un gráfico dinámico interactivo, el usuario puede seleccionar la moneda que desea analizar, y el sistema muestra la evolución del valor de conversión de esa moneda durante diferentes períodos.
+
+### La aplicación carga datos de archivos CSV específicos para cada moneda, incluyendo Guaraní Paraguayo, Peso Argentino, Peso Boliviano, Peso Chileno, Peso Uruguayo, Real Brasileño y Sol Peruano. Estos datos son visualizados mediante un gráfico de líneas interactivo, donde el eje X representa el período de tiempo y el eje Y muestra el valor de conversión de la moneda seleccionada en dólares estadounidenses.
+
+### El usuario puede seleccionar una moneda desde la barra lateral de la aplicación y explorar su comportamiento en el tiempo. La herramienta también incluye información de contacto para el desarrollador, brindando soporte directo.
---
+++ source/app.py
@@ -0,0 +1,78 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+
+def load_data():
+
+    try:
+        data = {
+            "Guaraní Paraguayo": pd.read_csv('source/archivosCSV/GuaraniParaguayo.csv', sep=','),
+            "Peso Argentino": pd.read_csv('source/archivosCSV/PesoArgentino.csv', sep=','),
+            "Peso Boliviano": pd.read_csv('source/archivosCSV/PesoBoliviano.csv', sep=','),
+            "Peso Chileno": pd.read_csv('source/archivosCSV/PesoChileno.csv', sep=','),
+            "Peso Uruguayo": pd.read_csv('source/archivosCSV/PesoUruguayo.csv', sep=','),
+            "Real Brasileño": pd.read_csv('source/archivosCSV/RealBrasileño.csv', sep=','),
+            "Sol Peruano": pd.read_csv('source/archivosCSV/SolPeruano.csv', sep=',')
+        }
+        return data
+    except Exception as e:
+        st.error(f"Error al cargar los datos: {str(e)}")
+        return {}
+    
+
+
+def main():
+
+    st.set_page_config(layout="wide")
+    
+    st.markdown(
+    """
+    <style>
+        div[data-baseweb="select"] > div {
+            cursor: pointer !important;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+
+    
+    data = load_data()
+    if not data:
+        return
+    
+    monedas = list(data.keys())
+    
+    moneda_seleccionada = st.sidebar.selectbox("Selecciona una moneda", monedas)
+
+    copyright_ = st.sidebar.text('© Renzo Reyna - Data Analyst / Python Developer')
+
+    telefono_ = st.sidebar.text('Teléfono: + 54 3525 - 620842 (Argentina)')
+    mail_ = st.sidebar.text('Email: desarrollador.sarmientino@gmail.com')
+    
+
+    
+    
+    df1 = data[moneda_seleccionada]
+    
+    
+    if 'Período analizado' in df1.columns and 'Valor de conversión' in df1.columns and 'Período analizado':
+        fig = px.line(df1, 
+                      x='Período analizado', 
+                      y='Valor de conversión', 
+                      title='Valor de monedas de Países Sudamericanos en USD ($ En centavos)'
+                      
+                      )
+        fig.update_layout(
+                        yaxis=dict(
+                            tickformat=",.6f"
+                        )
+                    )
+        
+        st.plotly_chart(fig)
+    else:
+        st.error("Los datos no tienen las columnas esperadas (Fecha y Valor). Verifica el formato de los archivos CSV.")
+
+if __name__ == "__main__":
+    main()
